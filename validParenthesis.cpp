67b68a8dae7d9e50d61893a90bef3832713eec1b
unknown <ajay766613@gmail.com>
2024-04-07 12:26:39 +0530

    valid parenthesis

---
+++ validParenthesis.cpp
@@ -0,0 +1,77 @@
+/*
+Given a string s containing only three types of characters: '(', ')' and '*', return true if s is valid.
+
+The following rules define a valid string:
+
+Any left parenthesis '(' must have a corresponding right parenthesis ')'.
+Any right parenthesis ')' must have a corresponding left parenthesis '('.
+Left parenthesis '(' must go before the corresponding right parenthesis ')'.
+'*' could be treated as a single right parenthesis ')' or a single left parenthesis '(' or an empty string "".
+
+
+Example 1:
+
+Input: s = "()"
+Output: true
+Example 2:
+
+Input: s = "(*)"
+Output: true
+Example 3:
+
+Input: s = "(*))"
+Output: true
+
+
+Constraints:
+
+1 <= s.length <= 100
+s[i] is '(', ')' or '*'.
+*/
+
+#include <string>
+using namespace std;
+
+class Solution
+{
+public:
+  bool checkValidString(string s)
+  {
+    int balance = 0;
+    for (char c : s)
+    {
+      if (c == '(' || c == '*')
+      {
+        balance++;
+      }
+      else
+      {
+        balance--;
+      }
+
+      // If balance is negative, it means there are more ')' than '(' and '*' encountered so far
+      if (balance < 0)
+        return false;
+    }
+
+    balance = 0;
+
+    for (int i = s.size() - 1; i >= 0; --i)
+    {
+      char c = s[i];
+      if (c == ')' || c == '*')
+      {
+        balance++;
+      }
+      else
+      {
+        balance--;
+      }
+
+      if (balance < 0)
+        return false;
+    }
+
+    return true;
+  }
+};
