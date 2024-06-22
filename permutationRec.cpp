bbdec0308832ce1872a6ad3d792b269ff05aeb24
unknown <ajay766613@gmail.com>
2024-06-23 00:29:20 +0530

    Permuation Recursion

---
+++ permutationRec.cpp
@@ -0,0 +1,54 @@
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+
+
+Example 1:
+
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+
+Input: nums = [1]
+Output: [[1]]
+
+
+Constraints:
+
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.
+*/
+class Solution
+{
+public:
+  vector<vector<int>> ans;
+
+  void recursion(int index, vector<int> &nums)
+  {
+    // Base case
+    if (index == nums.size())
+    {
+      ans.push_back(nums);
+      return;
+    }
+
+    // Recursive case
+    for (int i = index; i < nums.size(); i++)
+    {
+      swap(nums[i], nums[index]);
+      recursion(index + 1, nums);
+      swap(nums[i], nums[index]); // Backtrack
+    }
+  }
+
+  vector<vector<int>> permute(vector<int> &nums)
+  {
+    recursion(0, nums);
+    return ans;
+  }
+};
